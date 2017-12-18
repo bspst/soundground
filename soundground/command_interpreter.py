7a0ab59e4f89928a4af0621750ac03ac7f69aef4
Hizkia Felix <hizkifw@gmail.com>
2017-12-19 00:13:48 +0700

    Add command module

---
+++ soundground/command_interpreter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Command Interpreter
+Runs commands
+"""
+
+class Interpreter(object):
+    def __init__(self):
+        self.history = []
+
+    def execute(self, command):
+        pass
--- soundground/main.py
+++ soundground/main.py
@@ -7,8 +7,10 @@ from __future__ import print_function
 import argparse
 import sys
 import curses
+import curses.textpad
 
 from soundground import metadata, winman, utils
+from soundground import command_interpreter
 from soundground.winman import Value
 
 def main(stdscr):
@@ -18,24 +20,45 @@ def main(stdscr):
 
     # Initialize screen
     stdscr.clear()
+    stdscr.timeout(100)
     wg = winman.WindowGroup(stdscr)
+    ci = command_interpreter.Interpreter()
     
     # Create windows
     wg.create_window('title', 0, 0, 1, Value(100))
-    wg.create_window('status', Value(100, -1), 0, 1, Value(100))
     wg.create_window('nav', 1, 0, Value(100, -2), Value(25))
+    wg.create_window('command', Value(100, -1), 0, 1, Value(100))
 
+    # Set title bar style and contents
     wg['title'].bkgd(' ', curses.A_UNDERLINE)
-    wg['title'].addstr(0, 0, 'Soundground - v0.1')
+    wg['title'].addstr(0, 0, 'Soundground - v{}'.format(metadata.version))
+    # Left navigation pane
     wg['nav'].border(' ', '|', ' ', ' ', ' ', '|', ' ', '|')
+    # Command bar and textbox
+    wg['command'].bkgd(' ', curses.A_REVERSE)
+    commandbox = curses.textpad.Textbox(wg['command'])
 
     wg.resize()
 
+    last_command = ''
     while True:
+        # Check command box
+        if commandbox.gather() != last_command:
+            # New command was entered
+            last_command = commandbox.gather()
+            wg['command'].clear()
+            ci.execute(last_command)
+
         # Read input
         c = stdscr.getch()
         if c == ord('q'):
+            # Quit
             break
+        elif c == ord(':'):
+            # Enter command mode
+            wg['command'].clear()
+            wg['command'].addch(':')
+            commandbox.edit()
 
         # Check terminal resize
         if c == curses.KEY_RESIZE:
